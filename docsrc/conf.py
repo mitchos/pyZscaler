f422a5838db3f81861ae3263018a7b3add1f0e98
Mitch Kelly <me@mitchkelly.com.au>
2021-07-13 05:21:46 +1000

    Updates version to 0.1.1

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -27,7 +27,7 @@ author = 'Mitch Kelly'
 # The short X.Y version
 version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 
