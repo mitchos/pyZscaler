3ea0977d7961b2f61d52071aaba7a76dc33d41e4
Mitch Kelly <me@mitchkelly.com.au>
2021-07-26 16:21:55 +1000

    docs: adds docstring examples to audit logs

--- pyzscaler/zia/audit_logs.py
+++ pyzscaler/zia/audit_logs.py
@@ -2,17 +2,18 @@ from restfly.endpoint import APIEndpoint
 
 
 class AuditLogsAPI(APIEndpoint):
-
     def status(self):
         """
         Get the status of a request for an audit log report.
 
         Returns:
-            :obj:`dict`
-                Audit log report request status.
+            :obj:`dict`: Audit log report request status.
+
+        Examples:
+            >>> print(zia.audit_logs.status())
 
         """
-        return self._get('auditlogEntryReport')
+        return self._get("auditlogEntryReport")
 
     def create(self, start_time: str, end_time: str):
         """
@@ -21,39 +22,50 @@ class AuditLogsAPI(APIEndpoint):
         Creating a new audit log report will overwrite a previously-generated report.
 
         Args:
-            start_time:
+            start_time (str):
                 The timestamp, in epoch, of the admin's last login.
-            end_time:
+            end_time (str):
                 The timestamp, in epoch, of the admin's last logout.
 
         Returns:
-            :obj:`str`
-                The status code for the operation.
+            :obj:`str`: The status code for the operation.
+
+        Examples:
+            >>> zia.audit_logs.create(start_time='1627221600000',
+            ...    end_time='1627271676622')
 
         """
         payload = {
-            'startTime': start_time,
-            'endTime': end_time,
+            "startTime": start_time,
+            "endTime": end_time,
         }
-        return self._post('auditlogEntryReport', json=payload, box=False).status_code
+        return self._post("auditlogEntryReport", json=payload, box=False).status_code
 
     def cancel(self):
         """
         Cancels the request to create an audit log report.
 
         Returns:
-            :obj:`str`
-                The operation response code.
+            :obj:`str`: The operation response code.
+
+        Examples:
+            >>> zia.audit_logs.cancel()
 
         """
-        return self._delete('auditlogEntryReport', box=False).status_code
+        return self._delete("auditlogEntryReport", box=False).status_code
 
     def get_report(self):
         """
         Returns the most recently created audit log report.
 
         Returns:
-            :obj:`csv`
+            :obj:`str`: String representation of CSV file.
+
+        Examples:
+            Write report to CSV file:
+
+            >>> with open("audit_log.csv", "w+") as fh:
+            ...    fh.write(zia.audit_logs.get_report())
 
         """
-        return self._get('auditlogEntryReport/download', box=False).text
+        return self._get("auditlogEntryReport/download", box=False).text
