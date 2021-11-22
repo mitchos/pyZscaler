e803304ef30790208325f408663f9a5a95b947b6
Mitch Kelly <me@mkelly.dev>
2021-11-23 06:25:06 +1100

    fix: Fixes API call destination for list and get methods which was going to the wrong endpoint docs: Updates docstrings

--- pyzscaler/zpa/cloud_connector_groups.py
+++ pyzscaler/zpa/cloud_connector_groups.py
@@ -1,25 +1,35 @@
-from box import BoxList
+from box import Box, BoxList
 from restfly.endpoint import APIEndpoint
 
 from pyzscaler.utils import Iterator
 
 
 class CloudConnectorGroupsAPI(APIEndpoint):
-    def list_groups(self, **kwargs):
+    def list_groups(self, **kwargs) -> BoxList:
         """
         Returns a list of all configured cloud connector groups.
 
+        Keyword Args:
+            **max_items (int):
+                The maximum number of items to request before stopping iteration.
+            **max_pages (int):
+                The maximum number of pages to request before stopping iteration.
+            **pagesize (int):
+                Specifies the page size. The default size is 20, but the maximum size is 500.
+            **search (str, optional):
+                The search string used to match against features and fields.
+
         Returns:
-            :obj:`list`: A list of all configured cloud connector groups.
+            :obj:`BoxList`: A list of all configured cloud connector groups.
 
         Examples:
             >>> for cloud_connector_group in zpa.cloud_connector_groups.list_groups():
             ...    pprint(cloud_connector_group)
 
         """
-        return BoxList(Iterator(self._api, "cloudConnectorGroups", **kwargs))
+        return BoxList(Iterator(self._api, "cloudConnectorGroup", **kwargs))
 
-    def get_group(self, group_id: str):
+    def get_group(self, group_id: str) -> Box:
         """
         Returns information on the specified cloud connector group.
 
@@ -28,11 +38,11 @@ class CloudConnectorGroupsAPI(APIEndpoint):
                 The unique identifier for the cloud connector group.
 
         Returns:
-            :obj:`dict`: The resource record for the cloud connector group.
+            :obj:`Box`: The resource record for the cloud connector group.
 
         Examples:
-            >>> pprint(zpa.cloud_connector_groups.get_group('2342342342344433'))
+            >>> pprint(zpa.cloud_connector_groups.get_group('99999'))
 
         """
 
-        return self._get(f"cloudConnectorGroups/{group_id}")
+        return self._get(f"cloudConnectorGroup/{group_id}")
