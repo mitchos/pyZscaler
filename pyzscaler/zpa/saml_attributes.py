51d0a8795d8a996fab32284824c39276d0980532
Mitch Kelly <me@mkelly.dev>
2021-11-23 06:34:14 +1100

    refactor: Updates list methods to use the ZPA v2 API endpoints docs: Updates docstrings style: Adds argument annotation

--- pyzscaler/zpa/saml_attributes.py
+++ pyzscaler/zpa/saml_attributes.py
@@ -1,22 +1,68 @@
+from box import Box, BoxList
+from restfly import APISession
 from restfly.endpoint import APIEndpoint
+
+from pyzscaler.utils import Iterator
 
 
 class SAMLAttributesAPI(APIEndpoint):
-    def list_attributes(self):
+    def __init__(self, api: APISession):
+        super().__init__(api)
+
+        self.v2_url = api.v2_url
+
+    def list_attributes(self, **kwargs) -> BoxList:
         """
         Returns a list of all configured SAML attributes.
 
+        Keyword Args:
+            **max_items (int):
+                The maximum number of items to request before stopping iteration.
+            **max_pages (int):
+                The maximum number of pages to request before stopping iteration.
+            **pagesize (int):
+                Specifies the page size. The default size is 20, but the maximum size is 500.
+            **search (str, optional):
+                The search string used to match against features and fields.
+
         Returns:
-            :obj:`list`: A list of all configured SAML attributes.
+            :obj:`BoxList`: A list of all configured SAML attributes.
 
         Examples:
             >>> for saml_attribute in zpa.saml_attributes.list_attributes():
             ...    pprint(saml_attribute)
 
         """
-        return self._get("samlAttribute")
+        return BoxList(Iterator(self._api, f"{self.v2_url}/samlAttribute", **kwargs))
 
-    def get_attribute(self, attribute_id: str):
+    def list_attributes_by_idp(self, idp_id: str, **kwargs) -> BoxList:
+        """
+        Returns a list of all configured SAML attributes for the specified IdP.
+
+        Args:
+            idp_id (str): The unique id of the IdP to retrieve SAML attributes from.
+
+        Keyword Args:
+            **max_items (int):
+                The maximum number of items to request before stopping iteration.
+            **max_pages (int):
+                The maximum number of pages to request before stopping iteration.
+            **pagesize (int):
+                Specifies the page size. The default size is 20, but the maximum size is 500.
+            **search (str, optional):
+                The search string used to match against features and fields.
+
+        Returns:
+            :obj:`BoxList`: A list of all configured SAML attributes for the specified IdP.
+
+        Examples:
+            >>> for saml_attribute in zpa.saml_attributes.list_attributes_by_idp('99999'):
+            ...    pprint(saml_attribute)
+
+        """
+        return BoxList(Iterator(self._api, f"{self.v2_url}/samlAttribute/idp/{idp_id}", **kwargs))
+
+    def get_attribute(self, attribute_id: str) -> Box:
         """
         Returns information on the specified SAML attributes.
 
@@ -28,7 +74,7 @@ class SAMLAttributesAPI(APIEndpoint):
             :obj:`dict`: The resource record for the SAML attributes.
 
         Examples:
-            >>> pprint(zpa.saml_attributes.get_attribute('2342342342344433'))
+            >>> pprint(zpa.saml_attributes.get_attribute('99999'))
 
         """
 
