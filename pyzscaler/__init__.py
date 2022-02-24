00ab6558fdeed531aa4d1eec718cd51a398ce138
mkelly <me@mkelly.dev>
2022-02-24 22:06:17 +1100

    Bumps version to 1.0.1. Fixes #85

--- pyzscaler/__init__.py
+++ pyzscaler/__init__.py
@@ -4,7 +4,7 @@ __contributors__ = [
     "Dax Mickelson",
     "Jacob Gårder",
 ]
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from pyzscaler.zia import ZIA  # noqa
 from pyzscaler.zpa import ZPA  # noqa
