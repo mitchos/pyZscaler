65a1f2f910678374301f8189685ec89be7ff477b
mkelly <me@mkelly.dev>
2022-03-02 21:59:28 +1100

    Bumps version to 1.0.2. fix: Fixes #87, #88, #89, #90 chore: Resolves #77, #78, #79, #80, #84, #86 chore: Fixes typo in ZIA property docstring

--- pyzscaler/__init__.py
+++ pyzscaler/__init__.py
@@ -4,7 +4,7 @@ __contributors__ = [
     "Dax Mickelson",
     "Jacob Gårder",
 ]
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from pyzscaler.zia import ZIA  # noqa
 from pyzscaler.zpa import ZPA  # noqa
