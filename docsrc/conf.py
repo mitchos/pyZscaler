c32cae740cc90cafb3fbcffe487ce2c26206bc56
mkelly <me@mkelly.dev>
2022-04-14 10:16:24 +1000

    Bumps version to 1.1.1.

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -27,7 +27,7 @@ html_title = ""
 # The short X.Y version
 version = '1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.1'
 
 # -- General configuration ---------------------------------------------------
 
