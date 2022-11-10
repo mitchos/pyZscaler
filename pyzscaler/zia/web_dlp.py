4d12247a0cc9a59f4aa71e82855176cc3d33871f
daxm <3335783+daxm@users.noreply.github.com>
2022-11-10 13:48:04 -0700

    improve docstrings

--- pyzscaler/zia/web_dlp.py
+++ pyzscaler/zia/web_dlp.py
@@ -1,204 +1,80 @@
+from box import Box, BoxList
 from restfly.endpoint import APIEndpoint
+import requests
 
 
 class WebDLP(APIEndpoint):
-    def get_all(self, **kwargs):
-        """Gets a list of DLP policy rules, excluding SaaS Security API DLP policy rules."""
+    def get_all(self, **kwargs) -> BoxList:
+        """
+        Gets a list of DLP policy rules, excluding SaaS Security API DLP policy rules.
+
+        Returns:
+            BoxList of Web DLP items.
+        """
         return self._get("webDlpRules")
 
-    def get_item(self, item_id):
-        """Gets a DLP policy rule, excluding SaaS Security API DLP policy rules."""
+    def get_item(self, item_id) -> Box:
+        """
+        Gets a DLP policy rule, excluding SaaS Security API DLP policy rules.
+
+        Args:
+            item_id: String of ID.
+
+        Returns:
+            Box of Web DLP item.
+        """
         return self._get(f"webDlpRules/{item_id}")
 
-    def lite(self):
-        """Gets name and ID dictionary for all DLP policy rules, excluding SaaS Security API DLP policy rules."""
+    def lite(self) -> BoxList:
+        """
+        Gets name and ID dictionary for all DLP policy rules, excluding SaaS Security API DLP policy rules.
+
+        Returns:
+            BoxList of Web DLP name/ids.
+        """
         return self._get("webDlpRules/lite")
 
-    def post(self, payload):
-        """Adds a new DLP policy rule."""
-        """
-        {
-          "id": 0,
-          "order": 0,
-          "protocols": [
-            "ANY_RULE"
-          ],
-          "rank": 0,
-          "description": "string",
-          "locations": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
+    def post(self, payload) -> Box:
+        """Adds a new DLP policy rule.
+
+        Args:
+            payload: JSON of Web DLP Policy to POST.
+
+        Returns:
+            Box item of resulting POST.
+
+        Minimum items required in payload:
+            payload = {
+                'order': 1, # A number greater than 0.
+                'rank': 0,
+                'name': "Dax testing pyZscaler post.",
+                'protocols': ["ANY_RULE"],
+                'action': "ALLOW",
             }
-          ],
-          "locationGroups": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "groups": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "departments": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "users": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "urlCategories": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "dlpEngines": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "fileTypes": [
-            "ANY"
-          ],
-          "cloudApplications": [
-            "ANY"
-          ],
-          "minSize": 0,
-          "action": "ANY",
-          "state": "DISABLED",
-          "timeWindows": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "auditor": {
-            "id": 0,
-            "extensions": {
-              "additionalProp1": "string",
-              "additionalProp2": "string",
-              "additionalProp3": "string"
-            }
-          },
-          "externalAuditorEmail": "string",
-          "notificationTemplate": {
-            "id": 0,
-            "extensions": {
-              "additionalProp1": "string",
-              "additionalProp2": "string",
-              "additionalProp3": "string"
-            }
-          },
-          "matchOnly": true,
-          "lastModifiedTime": 0,
-          "lastModifiedBy": {
-            "id": 0,
-            "extensions": {
-              "additionalProp1": "string",
-              "additionalProp2": "string",
-              "additionalProp3": "string"
-            }
-          },
-          "icapServer": {
-            "id": 0,
-            "extensions": {
-              "additionalProp1": "string",
-              "additionalProp2": "string",
-              "additionalProp3": "string"
-            }
-          },
-          "withoutContentInspection": true,
-          "name": "string",
-          "labels": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "ocrEnabled": true,
-          "excludedGroups": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "excludedDepartments": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "excludedUsers": [
-            {
-              "id": 0,
-              "extensions": {
-                "additionalProp1": "string",
-                "additionalProp2": "string",
-                "additionalProp3": "string"
-              }
-            }
-          ],
-          "zscalerIncidentReciever": true
-        }
         """
         return self._post("webDlpRules", json=payload)
 
-    def put(self, item_id, payload):
+    def put(self, item_id, payload) -> Box:
+        """
+        Updates a DLP policy rule. This endpoint is not applicable to SaaS Security API DLP policy rules.
+
+        Args:
+            item_id: String of ID.
+            payload: JSON of Web DLP Policy to PUT.
+
+        Returns:
+            Box item of resulting PUT.
+        """
         return self._put(f"webDlpRules/{item_id}", json=payload)
 
-    def delete(self, item_id):
+    def delete(self, item_id) -> requests.Response:
+        """
+        Deletes a DLP policy rule. This endpoint is not applicable to SaaS Security API DLP policy rules.
+
+        Args:
+            item_id: String of ID.
+
+        Returns:
+                Requests.Response object.
+        """
         return self._delete(f"webDlpRules/{item_id}")
