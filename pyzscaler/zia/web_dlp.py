d4e18da0c71e7393dd55d5739510d6c7ee0babde
daxm <3335783+daxm@users.noreply.github.com>
2022-11-14 07:55:03 -0700

    Make changes as requested in PR

--- pyzscaler/zia/web_dlp.py
+++ pyzscaler/zia/web_dlp.py
@@ -1,42 +1,61 @@
 import json
 
-import requests
 from box import Box, BoxList
 from restfly.endpoint import APIEndpoint
 
 
 class WebDLP(APIEndpoint):
-    def get_all(self, **kwargs) -> BoxList:
+    def get_all_rules(self, **kwargs) -> BoxList:
         """
         Gets a list of DLP policy rules, excluding SaaS Security API DLP policy rules.
 
         Returns:
             BoxList of Web DLP items.
+
+        Examples:
+            # Get all Web DLP Items
+            results = zia.web_dlp.get_all_rules()
+            for item in results:
+                logging.info(item)
+
         """
         return self._get("webDlpRules")
 
-    def get_item(self, item_id: str) -> Box:
+    def get_rule(self, rule_id: str) -> Box:
         """
         Gets a DLP policy rule, excluding SaaS Security API DLP policy rules.
 
         Args:
-            item_id: String of ID.
+            rule_id: String of ID.
 
         Returns:
             Box of Web DLP item.
-        """
-        return self._get(f"webDlpRules/{item_id}")
 
-    def lite(self) -> BoxList:
+        Examples:
+            # Get Web DLP item by ID
+            results = zia.web_dlp.get_rule(rule_id='2671')
+            logging.info(results)
+
+        """
+        return self._get(f"webDlpRules/{rule_id}")
+
+    def get_rules_lite(self) -> BoxList:
         """
         Gets name and ID dictionary for all DLP policy rules, excluding SaaS Security API DLP policy rules.
 
         Returns:
             BoxList of Web DLP name/ids.
+
+        Examples:
+            # Get Web DLP Lite results
+            results = zia.web_dlp.get_rules_lite()
+            for item in results:
+                logging.info(item)
+
         """
         return self._get("webDlpRules/lite")
 
-    def post(self, payload: json) -> Box:
+    def add_rule(self, payload: json) -> Box:
         """Adds a new DLP policy rule.
 
         Args:
@@ -53,30 +72,60 @@ class WebDLP(APIEndpoint):
                 'protocols': ["ANY_RULE"],
                 'action': "ALLOW",
             }
+
+        Examples:
+            # Build minimum info for POST
+            payload = {
+                'order': 1,
+                'rank': 0,
+                'name': "Dax testing pyZscaler post.",
+                'protocols': ["ANY_RULE"],
+                'action': "ALLOW",
+            }
+
+            # Post new Web DLP item
+            results = zia.web_dlp.add_rule(payload=payload)
+            logging.info(results)
+            post_id = results['id']
+            logging.info(f"Posted payload result has id: {post_id}.")
+
         """
         return self._post("webDlpRules", json=payload)
 
-    def put(self, item_id: str, payload: json) -> Box:
+    def update_rule(self, rule_id: str, payload: json) -> Box:
         """
         Updates a DLP policy rule. This endpoint is not applicable to SaaS Security API DLP policy rules.
 
         Args:
-            item_id: String of ID.
+            rule_id: String of ID.
             payload: JSON of Web DLP Policy to PUT.
 
         Returns:
             Box item of resulting PUT.
-        """
-        return self._put(f"webDlpRules/{item_id}", json=payload)
 
-    def delete(self, item_id: str) -> requests.Response:
+        Examples:
+            # Update Web DLP item.
+            payload['name'] = "daxm updated name."
+            results = zia.web_dlp.update_rule(rule_id=post_id, payload=payload)
+            logging.info(results)
+
+        """
+        return self._put(f"webDlpRules/{rule_id}", json=payload)
+
+    def delete_rule(self, rule_id: str) -> int:
         """
         Deletes a DLP policy rule. This endpoint is not applicable to SaaS Security API DLP policy rules.
 
         Args:
-            item_id: String of ID.
+            rule_id: String of ID.
 
         Returns:
-                Requests.Response object.
+            Requests.Response object.
+
+        Examples:
+            # Delete Web DLP item.
+            results = zia.web_dlp.delete_rule(rule_id=post_id)
+            logging.info(results)
+
         """
-        return self._delete(f"webDlpRules/{item_id}")
+        return self._delete(f"webDlpRules/{rule_id}").status_code
