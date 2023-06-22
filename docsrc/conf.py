dcb61a76b87ad7358aa6be75f6ffffbac33d49e5
mkelly <me@mkelly.dev>
2023-06-22 12:52:25 +1000

    docs: updates intersphinx mappings
    Signed-off-by: mkelly <me@mkelly.dev>

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -182,7 +182,10 @@ epub_exclude_files = ["search.html"]
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
+                       'restfly': ('https://restfly.readthedocs.io/en/latest/', None),
+                       'box': ('https://box.readthedocs.io/en/latest', None),
+                       }
 
 # -- Options for todo extension ----------------------------------------------
 
